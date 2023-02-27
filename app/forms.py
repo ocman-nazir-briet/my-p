e3ac731ce0c5b6be85d6b7f1036dc350f9b3c79b
Sam Smith <usmannazirbareet@gmail.com>
2023-02-27 10:21:10 +0500

    added html forms in forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,9 @@ from .models import *
 from django import forms
 
 class authorForm(forms.ModelForm):
-    fname = forms.CharField(max_length = 200, help_text='Enter First Name: ')
-    lname = forms.CharField(max_length = 200, help_text='Enter Last Name: ')
-    email = forms.CharField(max_length = 200, help_text='Enter Email: ')
+    fname = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class':'border rounded-md text-sm outline-none lg:px-4 px-2 lg:py-3 py-0p6rem placeholder:text-A1A1A1 text-101928 focus:border-205a42 ease-in transition-all focus:border-l-8  bg-transparent', 'placeholder':'Enter your first name', 'autocomplete':'off'}))
+    lname = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class':'border rounded-md text-sm outline-none lg:px-4 px-2 lg:py-3 py-0p6rem placeholder:text-A1A1A1 text-101928 focus:border-205a42 ease-in transition-all focus:border-l-8  bg-transparent', 'placeholder':'Enter your last name', 'autocomplete':'off'}))
+    email = forms.EmailField(max_length=255, required=True, widget=forms.TextInput(attrs={'class':'border rounded-md text-sm outline-none lg:px-4 px-2 lg:py-3 py-0p6rem placeholder:text-A1A1A1 text-101928 focus:border-205a42 ease-in transition-all focus:border-l-8  bg-transparent', 'placeholder':'Enter your Email', 'autocomplete':'off'}))
     class Meta:
         model = Author
         fields = '__all__'
