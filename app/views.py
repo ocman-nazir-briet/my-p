cdcddfd12a74d1e8e475db5e93333de62c6f1313
Sam Smith <usmannazirbareet@gmail.com>
2023-02-27 11:57:03 +0500

    added schol model

--- app/views.py
+++ app/views.py
@@ -6,8 +6,11 @@ from django.contrib import messages
 
 def index(request):
     authors = Author.objects.all()
+    authorsCount = Author.objects.all().count()
+    print(authorsCount)
     form = authorForm()
     form1 = blogForm()
+    form2 = schoolForm()
     if request.method == 'POST':
         form = authorForm(request.POST)
         print(form)
@@ -16,13 +19,25 @@ def index(request):
             return redirect('/')
         else:
             messages.error(request, 'Something Went Wrong')
-    return render(request, 'index.html', {'authors':authors, 'form':form, 'form1':form1})
+    return render(request, 'index.html', {'authors':authors, 'form':form, 'form1':form1, 'form2':form2})
 
 def blog(request):
     if request.method =='POST':
-        form1 = blogForm(request.POST)
-        if form1.is_valid():
-            form1.save()
+        form = blogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            messages.error(request, 'Something Went Wrong')
+
+    return redirect('/')
+
+def school(request):
+    if request.method =='POST':
+        form = schoolForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
             return redirect('/')
         else:
             messages.error(request, 'Something Went Wrong')
